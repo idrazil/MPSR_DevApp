c7a078ef1da84e064340a2905f6e65e3a059257d
ano <arnaud.gonin@live.fr>
2020-07-17 22:23:54 +0200

    feature(Application): Add link between frontend and backend, application now retrieve coupons

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -2,6 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:qr_code_scanner/qr_scanner_overlay_shape.dart';
 
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+Future<Coupon> fetchCoupon(String couponId) async {
+  final response = await http.get(
+      'http://51.83.42.212:3000/coupons/' + couponId,
+      headers: {'Content-Type': 'application/json', 'charset': 'utf-8'});
+
+  if (response.statusCode == 200) {
+    return Coupon.fromJson(json.decode(response.body));
+  }
+  if (response.statusCode == 404) {
+    return Future.error(response.statusCode);
+  } else {
+    return Future.error(
+        response.statusCode, StackTrace.fromString(response.body));
+  }
+}
+
+class Coupon {
+  final String id;
+  final int value;
+  final String message;
+
+  Coupon({this.id, this.value, this.message});
+
+  factory Coupon.fromJson(Map<String, dynamic> json) {
+    return Coupon(
+      id: json['id'],
+      value: json['value'],
+      message: json['message'],
+    );
+  }
+}
+
 void main() {
   runApp(MaterialApp(home: MyApp()));
 }
@@ -15,6 +52,7 @@ class _MyAppState extends State<MyApp> {
   GlobalKey qrKey = GlobalKey();
   var qrText = "";
   QRViewController controller;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,20 +60,20 @@ class _MyAppState extends State<MyApp> {
         children: <Widget>[
           Expanded(
             flex: 5,
-             
-            child: QRView(key: qrKey, 
-              overlay: QrScannerOverlayShape(
-                borderRadius: 10,
-                borderColor: Colors.red,
-                borderLength: 30,
-                borderWidth: 10,
-                cutOutSize: 300,
-              ),
-              onQRViewCreated: _onQRViewCreated),
+            child: QRView(
+                key: qrKey,
+                overlay: QrScannerOverlayShape(
+                  borderRadius: 10,
+                  borderColor: Colors.red,
+                  borderLength: 30,
+                  borderWidth: 10,
+                  cutOutSize: 300,
+                ),
+                onQRViewCreated: _onQRViewCreated),
           ),
           Expanded(
             flex: 1,
-            child: Center(child: Text('Scan result: $qrText')),
+            child: Center(child: Text('$qrText')),
           )
         ],
       ),
@@ -49,10 +87,20 @@ class _MyAppState extends State<MyApp> {
   }
 
   void _onQRViewCreated(QRViewController controller) {
+    Future<Coupon> futureCoupon;
     this.controller = controller;
     controller.scannedDataStream.listen((scanData) {
+      futureCoupon = fetchCoupon(scanData);
+
       setState(() {
-        qrText = scanData;
+        futureCoupon.then(
+            (value) => {
+                  qrText = 'Réduction de ' + value.value.toString() + ' %'
+                }, onError: (e) {
+          qrText = 'Coupon Invalide';
+        });
+        // .then((value) => {qrText = value.message + ' %'})
+        // .catchError((handleError) => {log(handleError)});
       });
     });
   }
